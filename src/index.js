4649b39e803d468bd847a356692d0e84b2d8b484
Rohan Doshi <rohan.doshi02@gmail.com>
2022-05-04 15:31:58 +0530

    CORS Policy Changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const investorRouter = require("./routers/investor");
 const entrepreneurRouter = require("./routers/entrepreneur");
 const projectRouter = require("./routers/project");
 const coinsRouter = require("./routers/coins");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(investorRouter);
 app.use(entrepreneurRouter);
